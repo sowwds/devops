dd88d2a27f62c89cae1fa166aee4e5dd565e361c
sowwds <aleksey4094@yandex.ru>
2025-12-18 05:44:13 +0300

    update

--- lab8/todo-app/src/app.ts
+++ lab8/todo-app/src/app.ts
@@ -9,7 +9,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/todoapp';
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
