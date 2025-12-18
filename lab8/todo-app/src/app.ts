fdfbf031629e9a20726934ec0675485011455b89
sowwds <aleksey4094@yandex.ru>
2025-12-18 05:48:03 +0300

    update

--- lab8/todo-app/src/app.ts
+++ lab8/todo-app/src/app.ts
@@ -12,7 +12,6 @@ const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/todoapp';
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Connection
 mongoose.connect(MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
